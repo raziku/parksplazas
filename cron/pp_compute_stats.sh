9b0ffbe7ad9c61b5a2ca0b9c8ce49deecbccdf07
eddie.xie <oeddyo@gmail.com>
2013-12-07 16:21:22 -0500

    push

--- cron/pp_compute_stats.sh
+++ cron/pp_compute_stats.sh
@@ -1,4 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ..
+date
 nohup python stats.py >> ./log/stats.log&
--- cron/pp_transfer_data.sh
+++ cron/pp_transfer_data.sh
@@ -1,4 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ..
+date
 nohup python transfer_data.py >> ./log/transfer.log &
